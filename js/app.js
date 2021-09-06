3b8992c1dd1164fc06f8036d058e0809768246cf
Creative Lion <86979388+AndersonCreativeLion@users.noreply.github.com>
2021-09-06 20:52:27 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebAR.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Anchors.json");
 let WebXR;
 window.ARWT = {}
 
